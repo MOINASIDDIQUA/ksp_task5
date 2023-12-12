05f435d01af79b4133c9f7c1e44ad0a1024a9f50
MOINASIDDIQUA <143944638+MOINASIDDIQUA@users.noreply.github.com>
2023-12-12 17:59:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("let's go");
+
+/* operators
+ 1)arithmetic operators  (+,-,*,/,%,**)
+ 2)assignment operators  (+=,-=,*=,/=,%=)
+ 3)compartion operators   (<,>,<=,>=,==,===,!=)
+*/
+
+//arithmetic operators example
+let num1=35;
+let num2=20;
+const pi=3.14;
+
+console.log("35+20=",num1 + num2);
+console.log("35-∏=",num1 - pi);
+console.log("35/20=",num1 / num2);
+console.log("35*∏=",num1 * pi);
+console.log("35%20=",num1 % num2);
+console.log("35**25=",num1 ** num2);
+
+//assignment operators example
+let v1=34;
+let v2=43;
+console.log("v1+=v2=",v1+=v2);
+console.log("v1-=5=",v1-=5);
+console.log("v1*=3=",v1*=3);
+console.log("v1/=12=",v1/=12);
+console.log("v1%=v2=",v1%=v2);
+console.log("v1*=2=",v1*=2);
+
+//comparition oprators example
+let a=60;
+let b=12;
+
+console.log("a>b=",a>b);
+console.log("a<b=",a<b);
+console.log("a>=b=",a>=b);
+console.log("a<=b=",a<=b);
+console.log("a!=b=",a!=b);
+console.log("a==b=",a==b);
+console.log("a===b=",a===b);
+
+
+
